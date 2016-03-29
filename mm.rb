3765041b7bb6a90859c41d17e24ca425e0799b5b
Dinah <dinahrhandel@gmail.com>
2016-03-29 11:41:31 -0400

    Update mm.rb

--- mm.rb
+++ mm.rb
@@ -2,9 +2,8 @@ require 'formula'
 
 class Mm < Formula
   homepage 'http://github.com/mediamicroservices/mm'
-  url 'https://github.com/mediamicroservices/mm/archive/mm_v1.1.6.zip'
-  sha256 '118271d00c9f1b87ff8fb9ab87df02de9f1727e67e9dec5c0f8ecbd2e9c7ad21'
-  revision 4
+  url 'https://github.com/mediamicroservices/mm/archive/mm_v1.1.7.zip'
+  sha256 '76b59aba7f67c20c1b35169c61f3e92f549950947a7ae881c1423f82a08ad956'
   
   head 'git://github.com/mediamicroservices/mm.git'
 
@@ -50,7 +49,7 @@ class Mm < Formula
     bin.install "makeslate"
     bin.install "maketree"
     bin.install "makeyoutube"
-    bin.install "migrateaip"
+    bin.install "migratefiles"
     bin.install "mmconfig"
     bin.install "mmfunctions"
     bin.install "paperingest"
